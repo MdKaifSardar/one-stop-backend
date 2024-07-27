b2432934adc0a1a7b1d029ff50ff98ad41a77fab
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-07-27 16:44:20 +0530

    message added 1

--- controllers/productController.js
+++ controllers/productController.js
@@ -69,7 +69,7 @@ export const getAllProduct = async (req, res) => {
       .sort({ createdAt: -1 });
 
     const totalProducts = await productModel.countDocuments();
-    return res.status(200).json({
+    res.status(200).json({
       products: products,
       success: true,
       message: "Here are all the products",
@@ -77,7 +77,7 @@ export const getAllProduct = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({
+    res.status(500).json({
       error,
       success: false,
       message: "Something went wrong while gettin gthe product",

498e1687f44798b537e51c11f6fd8c3a246c2fde
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-07-27 14:25:45 +0530

    changed stuffs 6

--- Config/db.js
+++ Config/db.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB=async()=>{
-    try{
-        const conn=await mongoose.connect(process.env.MONGO_URL);
-
-        console.log('connected to mongodb');
-    }catch(err){
-        console.log(err);
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Database connected');
+    } catch (error) {
+        console.error('Database connection error:', error);
     }
-}
+};
 
 export default connectDB;

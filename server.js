9922c7aa8a6a8611526ed76b643816bbd524d2e4
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-07-27 11:43:20 +0530

    changed some stuffs

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import morgan from 'morgan';
 import connectDB from './Config/db.js';
 import authRoutes from './routes/authRoute.js'
@@ -7,9 +6,12 @@ import categoryRoute from './routes/categoryRoute.js'
 import productRoute from './routes/productRoute.js'
 import cors from 'cors';
 
+import dotenv from 'dotenv';
 dotenv.config();
 
-connectDB();
+const PORT= process.env.PORT || 8080;
+
+// connectDB();
 
 //rest object
 const app=express();
@@ -18,15 +20,13 @@ app.use(cors())
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/api/v1/auth',authRoutes)
-app.use('/api/v1/category',categoryRoute);
-app.use('/api/v1/product',productRoute);
+// app.use('/api/v1/auth',authRoutes)
+// app.use('/api/v1/category',categoryRoute);
+// app.use('/api/v1/product',productRoute);
 
 app.get('/',(req,res)=>{
     res.send("<h1>Welcom to the ecommerce app</h1>")
 })
-
-const PORT= process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
     console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`)

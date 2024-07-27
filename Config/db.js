e4bf18e17ad7e45df0b2c9a80e9e59f5d0a0e8c3
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-07-27 16:59:40 +0530

    db change

--- Config/db.js
+++ Config/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log('Database connected');
-    } catch (error) {
-        console.error('Database connection error:', error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 30000, // Increase timeout duration
+      connectTimeoutMS: 30000,
+    });
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
 };
 
 export default connectDB;

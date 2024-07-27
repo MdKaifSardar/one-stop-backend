cb1572f664cd4064b29c65842cda6299cf7be3d2
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-07-27 12:27:38 +0530

    changed stuffs 1

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 const express = require('express')
 const app = express();
-
-// import morgan from 'morgan';
-// import connectDB from './Config/db.js';
-// import authRoutes from './routes/authRoute.js'
-// import categoryRoute from './routes/categoryRoute.js'
-// import productRoute from './routes/productRoute.js'
-
-require('dotenv').config();
-// dotenv.config();
-
-// const PORT= process.env.PORT || 8080;
-
-// connectDB();
-
-//rest object
 app.use(express.json());
-// app.use(morgan('dev'));
-
-// app.use('/api/v1/auth',authRoutes)
-// app.use('/api/v1/category',categoryRoute);
-// app.use('/api/v1/product',productRoute);
 
 app.get('/',(req,res)=>{
     res.send("hello there hhe")
@@ -29,3 +9,24 @@ app.get('/',(req,res)=>{
 app.listen(5000, ()=>{
     console.log(`Server hehe running on port 5000`);
 })
+
+// import morgan from 'morgan';
+// import connectDB from './Config/db.js';
+// import authRoutes from './routes/authRoute.js'
+// import categoryRoute from './routes/categoryRoute.js'
+// import productRoute from './routes/productRoute.js'
+
+// require('dotenv').config();
+// dotenv.config();
+
+// const PORT= process.env.PORT || 8080;
+
+// connectDB();
+
+//rest object
+// app.use(express.json());
+// app.use(morgan('dev'));
+
+// app.use('/api/v1/auth',authRoutes)
+// app.use('/api/v1/category',categoryRoute);
+// app.use('/api/v1/product',productRoute);

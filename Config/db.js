18510c9f93b83d467404b4319447671d7ea1c8fa
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-07-27 14:56:25 +0530

    changed stuffs 8

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('Database connected');
     } catch (error) {
         console.error('Database connection error:', error);

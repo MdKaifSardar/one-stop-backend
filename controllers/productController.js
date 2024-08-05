59f8c11e204688ad642ed77e9084cf510baa184b
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-08-06 00:45:32 +0530

    modifien product search

--- controllers/productController.js
+++ controllers/productController.js
@@ -278,7 +278,7 @@ export const searchProduct = async (req, res) => {
   try {
     let totalProducts = 0;
     const { price, cat } = req.body;
-    const { keyword } = req.params;
+    const { keyword } = req.params || "";
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
 
